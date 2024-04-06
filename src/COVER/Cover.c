a6b5a488b5f59cd58fbfb1cfe57b7cdc6ad576a3
ryhoh <axis38akasira@gmail.com>
2024-04-07 01:55:43 +0900

    Cover実装
    初回実装

---
+++ src/COVER/Cover.c
@@ -0,0 +1,86 @@
+/**
+ * @file CoverControl.h
+ * @author ryhoh
+ * @brief フタの開閉検出ロジック
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+/* include *******************************************************************************************/
+#include "Cover.h"
+/* variable ******************************************************************************************/
+static u_int8_t zuc_COVER_OpenedFlag = Y_OFF;   /* [-,-] COVER_フタ開放フラグ */
+/* prototype *****************************************************************************************/
+static void COVER_Init(void);
+static void COVER_JudgeOpened(void);
+/* function ******************************************************************************************/
+/**
+ * @brief フタの開閉検出処理メイン
+ * 
+ */
+void COVER_Main(void) {
+    static uint8_t zuc_InitDoneFlag = Y_ON;     /* 初期化完了フラグ */
+
+    if (zuc_InitDoneFlag == Y_OFF) {            /* 初期化未完了時のみ実行 */
+        COVER_Init();                           /* 初期化処理 */
+        zuc_InitDoneFlag = Y_ON;                /* 初期化完了フラグをON */
+    }
+
+    /* フタの開閉検出処理 */
+    COVER_JudgeOpened();
+}
+
+/**
+ * @brief フタの開閉検出初期化処理
+ * 
+ */
+static void COVER_Init(void) {
+    /* センサにつながっているピンをセットアップする */
+    pinMode(PIN_COVER_SW_0, INPUT);             /* フタ開閉センサ0 */
+
+#if SW_COVER_SW_REDUNDANT == 1                  /* 冗長構成の場合 */
+    pinMode(PIN_COVER_SW_1, INPUT);             /* フタ開閉センサ1 */
+#endif
+}
+
+/**
+ * @brief フタの開閉検出処理
+ * 
+ */
+static void COVER_JudgeOpened(void) {
+    uint8_t uc_cover_sw_0;
+#if SW_COVER_SW_REDUNDANT == 1                  /* 冗長構成の場合 */
+    uint8_t uc_cover_sw_1;
+#endif
+
+    /* フタ開閉センサ0の状態を取得 */
+    uc_cover_sw_0 = digitalRead(PIN_COVER_SW_0);
+
+#if SW_COVER_SW_REDUNDANT == 1                  /* 冗長構成の場合 */
+    /* フタ開閉センサ1の状態を取得 */
+    uc_cover_sw_1 = digitalRead(PIN_COVER_SW_1);
+
+    /* 冗長構成の場合、どちらかが閉まっている（センサが通電しLOWとなる）とき、閉まっていると判定する */
+    if ((uc_cover_sw_0 == LOW) || (uc_cover_sw_1 == LOW)) {
+        uc_cover_sw_0 = Y_OFF;                  /* 閉まっている */
+    } else {
+        uc_cover_sw_0 = Y_ON;                   /* 開いている */
+    }
+#endif
+
+    /* 出力 */
+    zuc_COVER_OpenedFlag = uc_cover_sw_0;
+}
+
+/* Interface *****************************************************************************************/
+/**
+ * @brief COVER_フタ開放フラグ取得
+ * 
+ * @return COVER_フタ開放フラグ
+ */
+uint8_t COVER_GetOpenedFlag(void) {
+    return zuc_COVER_OpenedFlag;
+}
+
+/* ***************************************************************************************************/
---
+++ src/configure.h
@@ -0,0 +1,20 @@
+/**
+ * @file configure.c
+ * @author ryhoh
+ * @brief H/W Configurations
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+/* Compile Switch ***********************************************************************************/
+#define SW_COVER_SW_REDUNDANT           (1)         /* 0:No Redundunt 1:Redundunt                   */
+#define SW_BUZZER_NUM                   (4)         /* Number of buzzers installed                  */
+/* H/W Pin Assign ***********************************************************************************/
+#define PIN_COVER_SW_0                  (D9)         /* Cover Switch 0                              */
+#define PIN_COVER_SW_1                  (D10)        /* Cover Switch 1                              */
+#define PIN_BUZZER_0                    (D0)         /* Buzzer 0                                    */
+#define PIN_BUZZER_1                    (D1)         /* Buzzer 1                                    */
+#define PIN_BUZZER_2                    (D2)         /* Buzzer 2                                    */
+#define PIN_BUZZER_3                    (D3)         /* Buzzer 3                                    */
+/* **************************************************************************************************/
